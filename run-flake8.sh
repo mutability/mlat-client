0f366dceec2d30050ee5a2c09e93a5538ea5552c
Oliver Jowett <oliver@mutability.co.uk>
2015-05-08 22:58:43 +0100

    Exclude build/ from flake8 runs.

--- run-flake8.sh
+++ run-flake8.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 D=`dirname $0`
-python3 /usr/lib/python3/dist-packages/flake8/run.py --exclude=.git,__pycache__ mlat-client $D
+python3 /usr/lib/python3/dist-packages/flake8/run.py --exclude=.git,__pycache__,build mlat-client $D
