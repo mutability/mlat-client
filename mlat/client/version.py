6f00b2b8dc2e5053ded147b870867f1860a1ef74
Oliver Jowett <oliver@mutability.co.uk>
2015-05-09 10:07:46 +0100

    Add missing mlat.client.version module

---
+++ mlat/client/version.py
@@ -0,0 +1,21 @@
+# -*- mode: python; indent-tabs-mode: nil -*-
+
+# Part of mlat-client - an ADS-B multilateration client.
+# Copyright 2015, Oliver Jowett <oliver@mutability.co.uk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Just a version constant!"""
+
+CLIENT_VERSION = "0.1.12~dev"
